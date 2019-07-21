0be628c49c679cdaa59cd4083c323c153b6d0c15
senolerd <root@ovs-controller.underlay-network.com>
2019-07-21 14:11:52 -0400

    first try

---
+++ rocky/keystone.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+function installing(){
+  apt-get update 
+  apt-get install netcat mariadb-client
+  cd /tmp
+  git clone -b stable/rocky https://github.com/openstack/keystone.git
+  cd keystone
+  pip install -r requirements.txt
+  python setup.py install
+  echo "###########################"
+  echo "# PACKAGE INSTALL IS DONE #"
+  echo "###########################"
+
+}
+
+
+
+
+function keystone_create_db(){
+  mysql -u root -h $MYSQL_HOST -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE $KEYSTONE_DB_NAME;"
+  mysql -u root -h $MYSQL_HOST -p$MYSQL_ROOT_PASSWORD -e" GRANT ALL PRIVILEGES ON $KEYSTONE_DB_NAME.* TO '$KEYSTONE_DB_USER'@'%' IDENTIFIED BY '$KEYSTONE_USER_DB_PASS';"
+  mysql -u root -h $MYSQL_HOST -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON $KEYSTONE_DB_NAME.* TO '$KEYSTONE_DB_USER'@'localhost' IDENTIFIED BY '$KEYSTONE_USER_DB_PASS';"
+  unset -v MYSQL_ROOT_PASSWORD MYSQL_ALLOW_EMPTY_PASSWORD KEYSTONE_DB_NAME KEYSTONE_DB_USER KEYSTONE_USER_DB_PASS
+  }
+
+function bootstrap_pipeline(){ 
+	keystone_create_db
+	installing
+	echo "EoF bootstrap pipeline"
+	}
+
+function sql_connection_test(){ 
+	nc -z $MYSQL_HOST $MYSQL_PORT 
+	}
+
+function prepre_to_bootstrap(){
+  while true
+    do
+      if sql_connection_test
+        then
+          bootstrap_pipeline        
+          break
+      fi
+
+      echo "Connection test to sql server.. [ Last attempt: `date +%H:%M:%S` ]"
+      sql_connection_test
+      sleep 3
+    done
+}
+
+prepre_to_bootstrap
+echo "EoF BOOTSTRAPING"
+
+sleep 1d
