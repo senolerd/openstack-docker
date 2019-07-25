25c5f23a5442b4a64ed31fc96967fc7b68784964
senolerd <root@ovs-controller.underlay-network.com>
2019-07-25 14:38:39 -0400

    asdf

--- rocky/keystone/keystone.sh
+++ rocky/keystone/keystone.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+i#!/bin/bash
 # this line will be removed
 start=$(date +%s)
 
 function package_installing(){
   yum install -y centos-release-openstack-$OS_VERSION  python-openstackclient httpd mod_wsgi mariadb 
   yum install -y openstack-keystone
+  pip install python-openstackclient==3.19.0
   echo "# PACKAGE INSTALLING IS DONE     #"
 }
 
@@ -38,26 +39,42 @@ function check_permissions(){
 
 
 function keystone_setup(){
-
+  # Edit keystone.conf 
   sed -i "s|^\[database]|[database]\nconnection = mysql+pymysql://$KEYSTONE_DB_USER:$KEYSTONE_USER_DB_PASS@$MYSQL_HOST/$KEYSTONE_DB_NAME|g" /etc/keystone/keystone.conf
   sed -i "s|^\[token]|[token]\nprovider = fernet|g" /etc/keystone/keystone.conf
 
+  # Populate keystone db structure
   su -s /bin/sh -c "keystone-manage db_sync" keystone
 
   check_permissions
 
+  # Create Fernet keys
   keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
   keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
 
+  # Create keystone's public endpoint
   keystone-manage bootstrap --bootstrap-password adminpass \
   --bootstrap-public-url $KEYSTONE_PUBLIC_ENDPOINT \
+  --bootstrap-internal-url $KEYSTONE_INTERNAL_ENDPOINT \
   --bootstrap-region-id $KEYSTONE_REGION
 
+  
+  # Copy keystone's public endpoint wsgi conf
   ln -s /usr/share/keystone/wsgi-keystone.conf /etc/httpd/conf.d/
+  
+  # ryan the fire starter
+  # ryans_token=$(openstack token issue -f value  --os-auth-url $KEYSTONE_PUBLIC_ENDPOINT --os-project-domain-name Default --os-user-domain-name Default --os-project-name admin --os-password $ADMIN_PASS --os-username admin|grep '^gAAA')
 
 
-  systemctl enable httpd.service
-  systemctl start httpd.service
+  # openstack endpoint list --os-token $ryans_token --os-url $KEYSTONE_PUBLIC_ENDPOINT 
+
+
+
+
+
+
+
+
   }
 
 
