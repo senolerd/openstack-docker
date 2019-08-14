e6f8ce4ab226288dfe0ee08c6784eb33a9df286a
senolerd <senolerd@gmail.com>
2019-08-14 17:14:03 -0400

    new boot for keystone with multinode

--- glance.sh
+++ glance.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 started=$(date +%s)
+
 DOCKER_HOST_ADDR=$(echo "$DOCKER_HOST" |awk -F'//' {'print $2'}|awk -F':' {'print $1'})
+KEYSTONE_INTERNAL_ENDPOINT_TLS=$(echo "$KEYSTONE_INTERNAL_ENDPOINT_TLS" | tr '[:upper:]' '[:lower:]')
+if [ "$KEYSTONE_INTERNAL_ENDPOINT_TLS" == "true" ]
+  then KEYSTONE_PROTO="https"
+  else KEYSTONE_PROTO="http"
+fi
 
     yum install -y centos-release-openstack-$OS_VERSION  httpd mod_wsgi mariadb
     yum install -y openstack-glance python-openstackclient
@@ -45,12 +51,6 @@ function check_permissions(){
 function glance_api_setup(){
     INSECURE=$(echo "$INSECURE" | tr '[:upper:]' '[:lower:]')
     if [ "$INSECURE" == "true" ] ; then CERT_CHK=" --insecure ";fi
-
-    KEYSTONE_INTERNAL_ENDPOINT_TLS=$(echo "$KEYSTONE_INTERNAL_ENDPOINT_TLS" | tr '[:upper:]' '[:lower:]')
-    if [ "$KEYSTONE_INTERNAL_ENDPOINT_TLS" == "true" ]
-      then KEYSTONE_PROTO="https"
-      else KEYSTONE_PROTO="http"
-    fi
 
     echo "#### REMOVE ME: GLANCE KEYSTONE INTERNAL PROTO: $KEYSTONE_PROTO "
 
@@ -113,7 +113,7 @@ function glance_api_setup(){
 
     # openstack project create --domain default --description "Service Project" service
     echo "INFO [Glance]: CHECK SERVICE PROJECT IN CASE"
-    if openstack project show service --domain default $OS_ARGS
+    if openstack project show service --domain default $OS_ARGS > /dev/null 2>&1
       then echo "'service' project is exist"
       else openstack project create --domain default --description "Service Project" service $OS_ARGS
     fi
@@ -121,8 +121,8 @@ function glance_api_setup(){
 
     # Create service user
     echo "INFO [Glance]: Create service user glance user name"
-    if openstack user show $GLANCE_SERVICE_USERNAME $OS_ARGS
-      then "$GLANCE_SERVICE_USERNAME is exist"
+    if openstack user show $GLANCE_SERVICE_USERNAME $OS_ARGS > /dev/null 2>&1
+      then echo "$GLANCE_SERVICE_USERNAME is exist"
       else openstack user create --domain default --password $GLANCE_SERVICE_USER_PASS $GLANCE_SERVICE_USERNAME $OS_ARGS
     fi
 
@@ -132,7 +132,7 @@ function glance_api_setup(){
 
     # Glance service creation
     echo "INFO [Glance]: Glance service creation"
-    if openstack service show glance $OS_ARGS
+    if openstack service show glance $OS_ARGS > /dev/null 2>&1
       then echo "'glance' service exist."
       else openstack service create --name glance --description "OpenStack Image" image  $OS_ARGS
     fi
@@ -158,32 +158,25 @@ function glance_api_setup(){
 function server_configuration(){
     # DB Connection
 
-    keystone_authtoken="
-    [keystone_authtoken]
-    www_authenticate_uri  = $KEYSTONE_PROTO://$KEYSTONE_HOST:$KEYSTONE_INTERNAL_ENDPOINT_PORT
-    auth_url = $KEYSTONE_PROTO://$KEYSTONE_HOST:$KEYSTONE_INTERNAL_ENDPOINT_PORT/$KEYSTONE_INTERNAL_ENDPOINT_VERSION
-    memcached_servers = $MEMCACHED_HOST:$MEMCACHED_PORT
-    auth_type = password
-    project_domain_name = Default
-    user_domain_name = Default
-    project_name = service
-    username = $GLANCE_SERVICE_USERNAME
-    password = $GLANCE_SERVICE_USER_PASS
+    keystone_authtoken="\
+    \n[keystone_authtoken] \
+    \nwww_authenticate_uri  = $KEYSTONE_PROTO://$KEYSTONE_HOST:$KEYSTONE_INTERNAL_ENDPOINT_PORT \
+    \nauth_url = $KEYSTONE_PROTO://$KEYSTONE_HOST:$KEYSTONE_INTERNAL_ENDPOINT_PORT/$KEYSTONE_INTERNAL_ENDPOINT_VERSION \
+    \nmemcached_servers = $MEMCACHED_HOST:$MEMCACHED_PORT \
+    \nauth_type = password \
+    \nproject_domain_name = Default \
+    \nuser_domain_name = Default \
+    \nproject_name = service \
+    \nusername = $GLANCE_SERVICE_USERNAME \
+    \npassword = $GLANCE_SERVICE_USER_PASS \
     "
 
 
     for conf_file in /etc/glance/glance-api.conf /etc/glance/glance-registry.conf ;
       do
         sed -i "s|^\[database]|[database]\nconnection = mysql+pymysql://$GLANCE_DB_USER:$GLANCE_USER_DB_PASS@$MYSQL_HOST/$GLANCE_DB_NAME|g" $conf_file
-      done
-
-    for conf_file in /etc/glance/glance-api.conf /etc/glance/glance-registry.conf ;
-      do
         sed -i "s|^\[keystone_authtoken]|$keystone_authtoken|g" $conf_file
       done
-
-
-
 
 #    sed -i "s|^\[database]|[database]\nconnection = mysql+pymysql://$KEYSTONE_DB_USER:$KEYSTONE_USER_DB_PASS@$MYSQL_HOST/$KEYSTONE_DB_NAME|g" /etc/keystone/keystone.conf
 #    sed -i "s|^\[token]|[token]\nprovider = fernet\ncaching = true|g" /etc/keystone/keystone.conf
@@ -201,98 +194,10 @@ function server_configuration(){
 # DB POPULATE
 
 # MAIN
-glance_setup
+glance_api_setup
 server_configuration
 
 sleep 111d
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function check_permissions(){
-    chown -R root:keystone /etc/keystone
-    chmod 640 -R  /etc/keystone
-    chmod 750 /etc/keystone
-    echo "# INFO: PERMISSIONS CHECK IS DONE #"
-    }
-
-function keystone_setup(){
-    sed -i "s|^\[database]|[database]\nconnection = mysql+pymysql://$KEYSTONE_DB_USER:$KEYSTONE_USER_DB_PASS@$MYSQL_HOST/$KEYSTONE_DB_NAME|g" /etc/keystone/keystone.conf
-    sed -i "s|^\[token]|[token]\nprovider = fernet\ncaching = true|g" /etc/keystone/keystone.conf
-    sed -i "s|^\[cache]|[cache]\nenable = true \nbackend = dogpile.cache.memcached \nbackend_argument = url:$MEMCACHED_HOST:$MEMCACHED_PORT |g" /etc/keystone/keystone.conf
-    check_permissions
-    mkdir /etc/keystone/fernet-keys
-    mkdir /etc/keystone/credential-keys
-    chown keystone:keystone /etc/keystone/fernet-keys /etc/keystone/credential-keys
-    chmod 700 /etc/keystone/fernet-keys /etc/keystone/credential-keys
-    ln -s /run/secrets/fernet_0 /etc/keystone/fernet-keys/0
-    ln -s /run/secrets/fernet_1 /etc/keystone/fernet-keys/1
-    ln -s /run/secrets/credential_0 /etc/keystone/credential-keys/0
-    ln -s /run/secrets/credential_1 /etc/keystone/credential-keys/1
-    ln -s /usr/share/keystone/wsgi-keystone.conf /etc/httpd/conf.d/
-    }
-
-function populate_keystone(){
-    su -s /bin/sh -c "keystone-manage db_sync" keystone
-    keystone-manage bootstrap --bootstrap-password adminpass \
-    --bootstrap-public-url $KEYSTONE_PUBLIC_ENDPOINT \
-    --bootstrap-internal-url $KEYSTONE_INTERNAL_ENDPOINT \
-    --bootstrap-region-id $KEYSTONE_REGION
-  }
-
-function install_first_node(){
-    create_keystone_db
-    keystone_setup
-    populate_keystone
-    }
-
-function add_new_node(){
-    keystone_setup
-    }
-
-function main(){
-    while true
-        do
-          if  mysql -u root -h $MYSQL_HOST -p$MYSQL_ROOT_PASSWORD -e 'quit' ; then
-                echo "# INFO: SQL connected. $(date)"
-                if  mysql -u $KEYSTONE_DB_USER -h $MYSQL_HOST -p$KEYSTONE_USER_DB_PASS -e 'quit' 2> /dev/null ; then
-                  echo "# INFO: Installing additional api node."
-                  add_new_node
-                else
-                  echo "# INFO: Installing new api node node ."
-                  install_first_node
-                fi
-                break
-          else
-                echo "# INFO: Waiting for SQL server up.. Last trying time: $(date)"
-                sleep 1
-          fi
-        done
-    end=$(date +%s)
-    echo "# INFO: $OS_VERSION installing report: (started: $started, ended: $end, took $(expr $end - $start) secs )"
-    httpd -DFOREGROUND
-    }
-
-main
-
-
 
 
 
