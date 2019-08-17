8e9e3e0e8d7c1a3b3088b0271bb042b83c631ecd
senolerd <senolerd@gmail.com>
2019-08-17 03:57:46 -0400

    Opensack over Docker Swarm one click

--- branches/rocky/etc/create_cert.sh
+++ branches/rocky/etc/create_cert.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -config $base/etc/os_opnssl.cnf -keyout $base/etc/server.key -out $base/etc/server.crt
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -config $base/branches/$OS_VERSION//etc/os_opnssl.cnf -keyout $base/branches/$OS_VERSION//etc/server.key -out $base/branches/$OS_VERSION//etc/server.crt
